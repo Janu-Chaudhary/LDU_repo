46a4caabd023a2d6ca30ad6e59b89c270cf0c9f6
Janu-Chaudhary <januchaudhary2004@example.com>
2024-11-08 12:31:26 +0530

    Generation of the input matrix

---
+++ Ldu.js
@@ -0,0 +1,54 @@
+function generateMatrixInputs() {
+    const dimension = document.getElementById("dimension").value;
+  
+    if (dimension < 1) {
+      alert("Please enter a valid positive number.");
+      return;
+    }
+  
+    const matrix = document.getElementById("matrixInputContainer");
+    matrix.innerHTML = "";
+  
+    for (let i = 0; i < dimension; i++) {
+      const rowDiv = document.createElement("div");
+      rowDiv.className = "matrix-row";
+  
+      for (let j = 0; j < dimension; j++) {
+        const input = document.createElement("input");
+        input.type = "number";
+        input.className = "matrix-input";
+        input.placeholder = `A [${parseInt(i) + 1}][${[parseInt(j) + 1]}]`;
+        input.id = `cell-${i}-${j}`;
+        rowDiv.appendChild(input);
+      }
+      matrix.appendChild(rowDiv);
+    }
+  
+    document.getElementById("showMatrixBtn").style.display = "inline";
+  }
+
+  function displayMatrix() {
+    document.getElementById("line").style.display = "inline";
+    const dimension = document.getElementById("dimension").value;
+    const matrixContainer = document.getElementById("matrixContainer");
+    matrixContainer.innerHTML = "";
+  
+    const table = document.createElement("table");
+  
+    for (let i = 0; i < dimension; i++) {
+      const row = document.createElement("tr");
+  
+      for (let j = 0; j < dimension; j++) {
+        const cell = document.createElement("td");
+        const cellValue = document.getElementById(`cell-${i}-${j}`).value || 0;
+        cell.textContent = cellValue;
+        row.appendChild(cell);
+      }
+      table.appendChild(row);
+    }
+  
+    matrixContainer.appendChild(table);
+  
+    document.getElementById("showMatrixBtn").style.display = "none";
+    document.getElementById("Decompose").style.display = "inline";
+  }
